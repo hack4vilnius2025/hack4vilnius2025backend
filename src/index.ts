e6411497e34aa8cf72b9cdf784ed3148db72ee2f
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-10-18 23:38:51 +0300

    feat: add create petition endpoint to routes

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import authRoutes from './modules/auth/auth.routes';
 import usersRoutes from './modules/auth/users.routes';
 import forumApprovalsRoutes from './modules/forums/forum-approvals.routes';
 import forumsRoutes, { userForumsRouter } from './modules/forums/forums.routes';
+import petitionsRoutes from './modules/petitions/petitions.routes';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
@@ -39,7 +40,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/users', userForumsRouter); // Mount user forums routes at /api/users/forums
 app.use('/api/forums', forumsRoutes);
-app.use('/api/forums', forumApprovalsRoutes)
+app.use('/api/forums', forumApprovalsRoutes);
+app.use('/api/petitions', petitionsRoutes);
 
 AppDataSource.initialize()
   .then(() => {
