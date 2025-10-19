d9840ba8d8c9803e38ad48272d807a2dea624900
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-10-19 08:25:02 +0300

    feat: last one

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,10 +5,10 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'password',
+  host: process.env.STACKHERO_MYSQL_HOST || process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.STACKHERO_MYSQL_PORT || process.env.DB_PORT || '3306'),
+  username: 'root',
+  password: process.env.STACKHERO_MYSQL_ROOT_PASSWORD || process.env.DB_PASSWORD || 'password',
   database: process.env.DB_DATABASE || 'main',
   synchronize: true, // Set to false in production
   logging: process.env.NODE_ENV === 'development',
